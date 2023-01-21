d1df96c760d32d41b11fcc73063a23ceb701122a
mohadangKim <ehdlaos@gmail.com>
2023-01-22 00:34:33 +0900

    add prob1

---
+++ prob1/src/lib.rs
@@ -0,0 +1,173 @@
+pub struct Player {
+    pub health: u32,
+    pub mana: Option<u32>,
+    pub level: u32,
+}
+
+impl Player {
+    pub fn revive(&self) -> Option<Player> {
+        match self.health {
+            0 => {
+                Some(Player {
+                    health: 100 as u32,
+                    mana: if self.level < 10 {
+                        None
+                    } else {
+                        Some(100)
+                    },
+                    level: self.level,
+                })                
+            }
+            _ => {
+                println!("return NONE");
+                None
+            }            
+        }
+    }
+    pub fn cast_spell(&mut self, mana_cost: u32) -> u32 {
+        match self.mana {
+            None => {
+                self.health = if self.health < mana_cost {
+                    0
+                } else {
+                    self.health - mana_cost
+                };
+                0
+            }
+            Some(mana) => {
+                if mana < mana_cost {
+                    0
+                } else {
+                    self.mana = Some(mana - mana_cost);
+                    mana_cost * 2
+                }
+            }
+        }
+    }
+}
+
+#[test]
+fn test_reviving_dead_player() {
+    let dead_player = Player {
+        health: 0,
+        mana: Some(0),
+        level: 34,
+    };
+    let revived_player = dead_player
+        .revive()
+        .expect("reviving a dead player must return Some(player)");
+    assert_eq!(revived_player.health, 100);
+    assert_eq!(revived_player.mana, Some(100));
+    assert_eq!(revived_player.level, dead_player.level);
+}
+
+#[test]
+fn test_reviving_dead_level9_player() {
+    let dead_player = Player {
+        health: 0,
+        mana: None,
+        level: 9,
+    };
+    let revived_player = dead_player
+        .revive()
+        .expect("reviving a dead player must return Some(player)");
+    assert_eq!(revived_player.health, 100);
+    assert_eq!(revived_player.mana, None);
+    assert_eq!(revived_player.level, dead_player.level);
+}
+
+#[test]
+fn test_reviving_dead_level10_player() {
+    let dead_player = Player {
+        health: 0,
+        mana: Some(0),
+        level: 10,
+    };
+    let revived_player = dead_player
+        .revive()
+        .expect("reviving a dead player must return Some(player)");
+    assert_eq!(revived_player.health, 100);
+    assert_eq!(revived_player.mana, Some(100));
+    assert_eq!(revived_player.level, dead_player.level);
+}
+
+#[test]
+fn test_reviving_alive_player() {
+    let alive_player = Player {
+        health: 1,
+        mana: None,
+        level: 8,
+    };
+    assert!(alive_player.revive().is_none());
+}
+
+#[test]
+fn test_cast_spell_with_enough_mana() {
+    const HEALTH: u32 = 99;
+    const MANA: u32 = 100;
+    const LEVEL: u32 = 100;
+    const MANA_COST: u32 = 3;
+
+    let mut accomplished_wizard = Player {
+        health: HEALTH,
+        mana: Some(MANA),
+        level: LEVEL,
+    };
+
+    assert_eq!(accomplished_wizard.cast_spell(MANA_COST), MANA_COST * 2);
+    assert_eq!(accomplished_wizard.health, HEALTH);
+    assert_eq!(accomplished_wizard.mana, Some(MANA - MANA_COST));
+    assert_eq!(accomplished_wizard.level, LEVEL);
+}
+
+#[test]
+fn test_cast_spell_with_insufficient_mana() {
+    let mut no_mana_wizard = Player {
+        health: 56,
+        mana: Some(2),
+        level: 22,
+    };
+
+    let clone = Player { ..no_mana_wizard };
+
+    assert_eq!(no_mana_wizard.cast_spell(3), 0);
+    assert_eq!(no_mana_wizard.health, clone.health);
+    assert_eq!(no_mana_wizard.mana, clone.mana);
+    assert_eq!(no_mana_wizard.level, clone.level);
+}
+
+#[test]
+fn test_cast_spell_with_no_mana_pool() {
+    const MANA_COST: u32 = 10;
+
+    let mut underleveled_player = Player {
+        health: 87,
+        mana: None,
+        level: 6,
+    };
+
+    let clone = Player {
+        ..underleveled_player
+    };
+
+    assert_eq!(underleveled_player.cast_spell(MANA_COST), 0);
+    assert_eq!(underleveled_player.health, clone.health - MANA_COST);
+    assert_eq!(underleveled_player.mana, clone.mana);
+    assert_eq!(underleveled_player.level, clone.level);
+}
+
+#[test]
+fn test_cast_large_spell_with_no_mana_pool() {
+    const MANA_COST: u32 = 30;
+
+    let mut underleveled_player = Player {
+        health: 20,
+        mana: None,
+        level: 6,
+    };
+
+    assert_eq!(underleveled_player.cast_spell(MANA_COST), 0);
+    assert_eq!(underleveled_player.health, 0);
+    assert_eq!(underleveled_player.mana, None);
+    assert_eq!(underleveled_player.level, 6);
+}
